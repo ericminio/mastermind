4da99b068add665afb1f79a72cf9611c8a6f8a44
testaddict <eric@macbook-pro-de-eric-mignot.local>
2010-06-03 00:03:58 +0200

    first import

---
+++ src/Mastermind.java
@@ -0,0 +1,15 @@
+
+public class Mastermind {
+
+	public String secret;
+	
+	public void enterRow() {
+		
+	}
+	
+	public String proposition;
+	
+	public int nombreDeCouleursBienPlacees() {
+		return 0;
+	}
+}
